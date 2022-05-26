486d4f2a30533c39e32516dff219edcbda088fe6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-27 08:55:29 +0900

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Toshiyana on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
