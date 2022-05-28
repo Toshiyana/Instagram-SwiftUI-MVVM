cf31e18f5cb83b1d00fc8c9c5392f35f256d04be
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-28 18:50:47 +0900

    feat: add UploadPostView

--- InstagramSwiftUI/View/UploadPost/UploadPostView.swift
+++ InstagramSwiftUI/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("UploadPost")
+        VStack {
+            if postImage == nil {
+                Button(action: {}, label: {
+                    Image("plus_photo")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("venom-7")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption..", text: $captionText)
+                }.padding()
+                
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }).padding()
+            }
+            Spacer()
+        }
     }
 }
 
