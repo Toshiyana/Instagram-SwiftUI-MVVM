a8c07e10f3e110a861a2bb28f6921befe1d69724
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-29 03:10:42 +0900

    feat: add LoginView

---
+++ InstagramSwiftUI/View/Authentication/ResetPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ResetPasswordView.swift
+//  InstagramSwiftUI
+//
+//  Created by Toshiyana on 2022/05/29.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResetPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResetPasswordView()
+    }
+}
