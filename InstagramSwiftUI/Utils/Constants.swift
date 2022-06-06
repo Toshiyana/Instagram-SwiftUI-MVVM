27986dc5a91fe90e49dc338158a5035576221048
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-06 13:39:59 +0900

    feat: Fetching the currently logged in user

---
+++ InstagramSwiftUI/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  InstagramSwiftUI
+//
+//  Created by Toshiyana on 2022/06/06.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
