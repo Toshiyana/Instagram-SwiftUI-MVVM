3b894913b79ea1e955cf1abdb9b49e4c114ceb33
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-06 16:59:27 +0900

    feat: Filtering users with search bar

--- InstagramSwiftUI/ViewModel/Feed/SearchViewModel.swift
+++ InstagramSwiftUI/ViewModel/Feed/SearchViewModel.swift
@@ -31,4 +31,10 @@ class SearchViewModel: ObservableObject {
             print(self.users)
         }
     }
+    
+    func filteredUsers(_ query: String) -> [User] {
+        let lowercasedQuery = query.lowercased()
+        return users.filter({ $0.fullname.lowercased().contains(lowercasedQuery) ||
+            $0.username.lowercased().contains(lowercasedQuery) })
+    }
 }
