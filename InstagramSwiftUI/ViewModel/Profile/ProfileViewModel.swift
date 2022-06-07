8db8dbe5f5fcbc61b3d7d0f8da2f9adfe21ec1fe
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-07 16:21:43 +0900

    feat: Unfollowing & Checking to see if a user is followed

--- InstagramSwiftUI/ViewModel/Profile/ProfileViewModel.swift
+++ InstagramSwiftUI/ViewModel/Profile/ProfileViewModel.swift
@@ -12,6 +12,7 @@ class ProfileViewModel: ObservableObject {
     
     init(user: User) {
         self.user = user
+        checkIfUserIsFollowed()
     }
     
     func follow() {
@@ -22,10 +23,17 @@ class ProfileViewModel: ObservableObject {
     }
     
     func unfollow() {
-        print("DEBUG: Unfollow user..")
+        guard let uid = user.id else { return }
+        UserService.unfollow(uid: uid) { _ in
+            self.user.isFollowed = false
+        }
     }
     
     func checkIfUserIsFollowed() {
-        
+        guard !user.isCurrentUser else { return } // make sure that user is not the currentUser
+        guard let uid = user.id else { return }
+        UserService.checkIfUserIsFollowed(uid: uid) { isFollowed in
+            self.user.isFollowed = isFollowed
+        }
     }
 }
