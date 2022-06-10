f9469fb94c246f3a9985bdac9703f4e03ab3c193
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-10 21:46:09 +0900

    feat: Uploading a comment to firestore

--- InstagramSwiftUI/View/Comments/CommentsView.swift
+++ InstagramSwiftUI/View/Comments/CommentsView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct CommentsView: View {
     @State var commentText = ""
+    @ObservedObject var viewModel: CommentViewModel
+    
+    init(post: Post) {
+        self.viewModel = CommentViewModel(post: post)
+    }
     
     var body: some View {
         VStack {
@@ -27,12 +32,7 @@ struct CommentsView: View {
     }
     
     func uploadComment() {
-        
-    }
-}
-
-struct CommentsView_Previews: PreviewProvider {
-    static var previews: some View {
-        CommentsView()
+        viewModel.uploadComment(commentText: commentText)
+        commentText = ""
     }
 }
