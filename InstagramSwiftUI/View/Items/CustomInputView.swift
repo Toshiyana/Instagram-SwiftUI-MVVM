08757273cedbc13e01d6abe8cc0ba4c695f57a48
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-10 20:45:05 +0900

    feat: Custom comment input view

---
+++ InstagramSwiftUI/View/Items/CustomInputView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomInputView.swift
+//  InstagramSwiftUI
+//
+//  Created by Toshiyana on 2022/06/10.
+//
+
+import SwiftUI
+
+struct CustomInputView: View {
+    @Binding var inputText: String
+    
+    var action: () -> Void // define action insted of using viewModel pattern (different way)
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .foregroundColor(Color(.separator))
+                .frame(width: UIScreen.main.bounds.width, height: 0.75)
+                .padding(.bottom, 8)
+            
+            HStack {
+                TextField("Comment...", text: $inputText)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .frame(minWidth: 30)
+                
+                Button(action: action) {
+                    Text("Send")
+                        .bold()
+                        .foregroundColor(.black)
+                }
+            }
+        }
+        .padding(.bottom, 8)
+        .padding(.horizontal)
+    }
+}
