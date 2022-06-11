6529573122b1e9d310e49b4abdc40c7b1c8af4bd
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-11 13:18:57 +0900

    feat: Notification Data Model

---
+++ InstagramSwiftUI/Model/Notification.swift
@@ -0,0 +1,33 @@
+//
+//  Notification.swift
+//  InstagramSwiftUI
+//
+//  Created by Toshiyana on 2022/06/11.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var postId: String?
+    let username: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+    case like
+    case comment
+    case follow
+    
+    var notificationMessage: String {
+        switch self {
+        case .like: return " liked one of your posts."
+        case .comment: return " commented on one of your posts."
+        case .follow: return " started following you."
+        }
+    }
+}
